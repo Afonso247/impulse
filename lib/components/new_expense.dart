862c4a142953540a3bf07c33312919dcbee853fa
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-09-29 20:23:46 -0300

    feat: implementando TextField e TextEditingController

--- lib/components/new_expense.dart
+++ lib/components/new_expense.dart
@@ -8,8 +8,37 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('Cadastrar despesa'),
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(labelText: 'Título'),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () => debugPrint(_titleController.text),
+                child: const Text('Salvar'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
